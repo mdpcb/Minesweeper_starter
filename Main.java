fc1a709a1db639207cc2258e960fa5afa9bcbb01
Phillip Blake <68248890+mdpcb@users.noreply.github.com>
2024-11-06 12:54:28 -0600

    Update Main.java
    Add ANSI codes for color output

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
 public class Main{
+  private static final String ANSI_RESET = "\u001B[0m";
+  private static final String ANSI_RED = "\u001B[31m";
+  private static final String ANSI_BLUE = "\u001B[34m";
+  private static final String ANSI_GRAY = "\u001B[90m";
+
+  private static final char FILLED_BLOCK = '\u2588';
+  
   public static void main(String[] args){
   }
 }
